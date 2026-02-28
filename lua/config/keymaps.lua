56afb13090f820e46205df6a4e3a7fb2852d5607
Veikko Hiltunen <veikko.hiltun@gmail.com>
2026-02-28 14:10:07 +0200

    Modify nvim configs and keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,47 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
+-- ~/.config/nvim/lua/config/keymaps.lua
+local map = vim.keymap.set
+
+vim.g.mapleader = " "
+
+-- Select all
+map("n", "<leader>a", "ggVG", { desc = "Select all" })
+
+-- Redo on U
+map("n", "U", "<C-r>", { desc = "Redo" })
+
+-- Keep Q as format (like your old vimrc)
+map("n", "Q", "gq", { desc = "Format operator" })
+
+-- Centered scrolling + search
+map("n", "<C-d>", "<C-d>zz", { desc = "Half page down (center)" })
+map("n", "<C-u>", "<C-u>zz", { desc = "Half page up (center)" })
+map("n", "n", "nzzzv", { desc = "Next search (center)" })
+map("n", "N", "Nzzzv", { desc = "Prev search (center)" })
+
+-- Clipboard yanks like IDEAVim
+map({ "n", "v" }, "<leader>y", '"+y', { desc = "Yank to system clipboard" })
+map("n", "<leader>Y", '"+Y', { desc = "Yank line to system clipboard" })
+
+-- Paste from system clipboard (normal)
+map("n", "<C-v>", '"+p', { desc = "Paste system clipboard" })
+
+-- Paste in insert mode (Shift+Ctrl+V)
+map("i", "<C-S-v>", "<C-r>+", { desc = "Paste system clipboard (insert)" })
+map("v", "<C-S-v>", '"+p', { desc = "Paste system clipboard (visual)" })
+
+-- Paste over selection without overwriting register
+map("v", "<leader>p", '"_dP', { desc = "Paste (keep register)" })
+
+-- Delete without yanking
+map({ "n", "v" }, "<leader>d", '"_d', { desc = "Delete (no yank)" })
+
+-- Indent keeps selection
+map("v", "<", "<gv", { desc = "Indent left (keep select)" })
+map("v", ">", ">gv", { desc = "Indent right (keep select)" })
+
+-- Move selected lines up/down (same as your ideavim)
+map("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move selection down" })
+map("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move selection up" })
+
+-- Reload config quickly
+map("n", "<leader>src", ":source $MYVIMRC<CR>", { desc = "Source vimrc" })
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,34 +1,46 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
 vim.opt.guicursor = ""
 
-vim.opt.nu = true
+vim.opt.number = true
 vim.opt.relativenumber = true
 
+-- Tabs / indent (keep yours)
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
-
 vim.opt.smartindent = true
 
 vim.opt.wrap = false
 
+-- Undo / swap (keep yours)
 vim.opt.swapfile = false
 vim.opt.backup = false
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
 
-vim.opt.hlsearch = false
+-- Search behavior (IDEAVim-like)
 vim.opt.incsearch = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.hlsearch = false -- set true if you want matches highlighted after search
 
+-- UI
 vim.opt.termguicolors = true
-
-vim.opt.scrolloff = 8
+vim.opt.scrolloff = 15 -- IDEAVim had 15
 vim.opt.signcolumn = "yes"
-vim.opt.isfname:append("@-@")
-
-vim.opt.updatetime = 50
-
 vim.opt.colorcolumn = "80"
+
+-- Keymap timeout (IDEAVim had timeoutlen=300)
+vim.opt.timeout = true
+vim.opt.timeoutlen = 300
+
+-- Clipboard (IDE-ish)
+vim.opt.clipboard = "unnamedplus"
+
+-- Misc (keep yours)
+vim.opt.isfname:append("@-@")
+vim.opt.updatetime = 50
